cae5df7cb075570e817a9bcb47b9c127783bf334
Alannah Bennett <alannahmb2004@gmail.com>
2023-09-10 21:28:35 +0100

    Created menu for ATM project

---
+++ banking.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    int pin = 1234;
+    int user_pin,menu_choice;
+
+	printf("Welcome to the ATM Machine \n Please choose from the options below:");
+    printf("1.Enter Pin \n 2. Change Pin \n 3. Times PIN entered \n 4. Exit \n");
+
+    scanf("%d",&menu_choice);;
+
+
+    // if(pin == user_pin)
+    // {
+    //     printf("")
+        
+    // }
+    // else {
+    //     printf("ERROR: Wrong PIN");
+    // }
+
+	// return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+    // if(pin == user_pin)
+    // {
+    //     printf("")
+        
+    // }
+    // else {
+    //     printf("ERROR: Wrong PIN");
+    // }
+
+	// return 0;
