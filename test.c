c6e0d6e75584d2a1384db2c8b3eaf6b1fa51f784
Alannah Bennett <alannahmb2004@gmail.com>
2023-09-10 15:26:43 +0100

    first commit

---
+++ test.c
@@ -0,0 +1,23 @@
+//QUICK QUIZ:
+// write a program to print the first n
+//natural numbers using do while loop
+
+//QUICK QUIZ:
+// write a program to print the first n
+//natural numbers using do while loop
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	int n=4;
+    do
+    {
+        printf("number is: %d \n", n);
+        n--;
+    } while (n>=1);
+    
+
+	return 0;
+}
